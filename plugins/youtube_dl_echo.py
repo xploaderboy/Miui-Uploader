5a45c9e7d563920658d25a31e8c22b456a6aa76c
Vinayak703 <76476634+vinayakext2@users.noreply.github.com>
2021-04-09 19:16:30 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -150,7 +150,7 @@ async def echo(bot, update):
             reply_to_message_id=update.message_id,
             parse_mode="html",
             disable_web_page_preview=True
-            logger.info(f"Error:{error_message}")
+            logger.info(error_message)
         )
         return False
     if t_response:
