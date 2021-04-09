80aab4a34000fab8f0ae38a9ea03283c5d2ad853
Vinayak703 <76476634+vinayakext2@users.noreply.github.com>
2021-04-09 18:44:50 +0530

    Update private_filter.py

--- plugins/private_filter.py
+++ plugins/private_filter.py
@@ -10,4 +10,4 @@ else:
     
 @pyrogram.Client.on_message(pyrogram.filters.text & (filters.private))
 async def private(client, message):
-    await message.reply_text("This bot will only work in MeMeUi Mirrot \n Join The Group Now \nhttps://t.me/MemeUi_Mirror")
+    await message.reply_text("This bot will only work in MeMeUi Mirror \n Join The Group Now \nhttps://t.me/MemeUi_Mirror")
