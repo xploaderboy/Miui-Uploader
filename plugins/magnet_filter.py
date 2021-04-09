0541cd1b4413e79b796104164c390f9d4b4b5e79
Vinayak703 <76476634+vinayakext2@users.noreply.github.com>
2021-04-09 18:56:59 +0530

    Update magnet_filter.py

--- plugins/magnet_filter.py
+++ plugins/magnet_filter.py
@@ -3,6 +3,6 @@ import asyncio
 from datetime import datetime
 from pyrogram import Client, filters
 
-@Client.on_message(filters.regex("magnet"))
+@Client.on_message(filters.regex("/upload@xploaderzxbot magnet"))
 async def command(client, message):
-    await message.reply_text("Bla Bla Bla")
+    await message.reply_text("For Magnet File Leeching \nUse Our Jarvis Uploader")
